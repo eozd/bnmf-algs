434843a1fe24130aa0985046ec264de8df5cabb6
Esref Ozdemir <esref.ozdemir27@gmail.com>
2018-02-22 10:53:22 +0300

    Log marginal of S function declaration and doc

--- include/sampling.hpp
+++ include/sampling.hpp
@@ -53,4 +53,32 @@ namespace bnmf_algs {
     tensor3d_t sample_S(const matrix_t& prior_W,
                         const matrix_t& prior_H,
                         const vector_t& prior_L);
+
+
+    /**
+     * @brief Compute the log marginal of tensor S with respect to the given
+     * distribution parameters.
+     *
+     * According to the Bayesian NMF allocation model \cite kurutmazbayesian,
+     *
+     * \f$\log{p(S)} = \log{p(W, H, L, S)} - \log{p(W, H, L | S)}\f$.
+     *
+     * @param S Tensor \f$S\f$ of size \f$x \times y \times z \f$.
+     * @param a Shape parameter of Gamma distribution.
+     * @param b Rate parameter of Gamma distribution.
+     * @param alpha Parameter vector of Dirichlet prior for matrix \f$W\f$ of size \f$x\f$.
+     * @param beta Parameter vector of Dirichlet prior for matrix \f$H\f$ of size \f$z\f$.
+     *
+     * @return log marginal \f$\log{p(S)}\f$.
+     *
+     * @remark Number of alpha parameters must be \f$x\f$.
+     * @remark Number of beta parameters must be \f$z\f$.
+     *
+     * @author Esref Ozdemir
+     */
+    double log_marginal_S(const tensor3d_t& S,
+                          double a,
+                          double b,
+                          const std::vector<double>& alpha,
+                          const std::vector<double>& beta);
 }
