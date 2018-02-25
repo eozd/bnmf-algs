15d02a04d779902f7752f7ad22b5ef4e2956f485
Esref Ozdemir <esref.ozdemir27@gmail.com>
2018-02-25 14:10:28 +0300

    Implement wrapper around gsl_ran_discrete_t

--- include/wrappers.hpp
+++ include/wrappers.hpp
@@ -10,11 +10,40 @@ namespace bnmf_algs {
 using gsl_rng_wrapper = std::unique_ptr<gsl_rng, decltype(&gsl_rng_free)>;
 
 /**
+ * @brief RAII wrapper around gsl_rng_discrete types.
+ */
+using gsl_ran_discrete_wrapper =
+    std::unique_ptr<gsl_ran_discrete_t, decltype(&gsl_ran_discrete_free)>;
+
+/**
  * @brief Construct a new bnmf_algs::gsl_rng_wrapper from the given rng type.
  *
  * @param T Random number generator type.
  *
  * @return bnmf_algs::gsl_rng_wrapper type containing a gsl_rng in it.
+ *
+ * @see gsl_rng_alloc.
  */
 gsl_rng_wrapper make_gsl_rng(const gsl_rng_type* T);
+
+/**
+ * @brief Construct a new bnmf_algs::gsl_ran_discrete_wrapper.
+ *
+ * From gsl_ran_discrete_preproc documentation:
+ *
+ * "
+ * The array P contains the probabilities of the discrete events; these array
+ * elements must all be positive, but they needn’t add up to one (so you can
+ * think of them more generally as “weights”)—the preprocessor will normalize
+ * appropriately.
+ * "
+ *
+ * @param K Number of objects to choose from.
+ * @param P Pointer to an array containing the probability of \f$i^{th}\f$ item at P[i].
+ *
+ * @return gsl_ran_discrete_wrapper wrapping a gsl_ran_discrete_t object.
+ *
+ * @see gsl_ran_discrete_preproc
+ */
+gsl_ran_discrete_wrapper make_gsl_ran_discrete(size_t K, const double* P);
 } // namespace bnmf_algs
--- src/wrappers.cpp
+++ src/wrappers.cpp
@@ -5,3 +5,8 @@ bnmf_algs::gsl_rng_wrapper bnmf_algs::make_gsl_rng(const gsl_rng_type* T) {
     return gsl_rng_wrapper(gsl_rng_alloc(T), gsl_rng_free);
 }
 
+bnmf_algs::gsl_ran_discrete_wrapper
+bnmf_algs::make_gsl_ran_discrete(size_t K, const double* P) {
+    return gsl_ran_discrete_wrapper(gsl_ran_discrete_preproc(K, P),
+                                    gsl_ran_discrete_free);
+}
