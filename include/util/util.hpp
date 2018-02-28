937a3e2c79323b703511ef533c377d8ce6e99a48
Esref Ozdemir <esref.ozdemir27@gmail.com>
2018-02-28 13:57:53 +0300

    Implement template tensor normalization function
    Tensors of arbitrary dimensions can be normalized along a given single
    axis using L1, L2 and L-infty norms.

--- include/util/util.hpp
+++ include/util/util.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
+#include "defs.hpp"
 #include <cstddef>
 #include <utility>
-#include "defs.hpp"
 
 namespace bnmf_algs {
 /**
@@ -88,5 +88,72 @@ template <typename Function, typename Tuple> auto call(Function f, Tuple t) {
  * sampling
  */
 double sparseness(const tensord<3>& S);
+
+/**
+ * @brief Type of the norm to be used by bnmf_algs::util::normalize and
+ * bnmf_algs::util::normalized.
+ */
+enum class NormType {
+    L1, ///< Normalize using L1-norm (divide by sums of absolute values)
+    L2, ///< Normalize using L2-norm (divide by sqrt of sum of squares)
+    Max }; ///< Normalize using L-infinity norm (divide by max of absolute values)
+
+/**
+ * @brief Normalize a tensor in-place along the given axis.
+ *
+ * @tparam N Dimension of the tensor. Must be known at compile time.
+ * @param input Input tensor to normalize in-place.
+ * @param axis Axis along which the tensor will be normalized.
+ * @param type Normalization type. See bnmf_algs::util::NormType for details.
+ * @param eps Floating point epsilon value to prevent division by 0 errors.
+ */
+template <int N>
+void normalize(tensord<N>& input, size_t axis, NormType type = NormType::L1,
+               double eps = 1e-50) {
+    static_assert(N >= 1, "Tensor must be at least 1 dimensional");
+
+    tensord<N - 1> norms;
+    shape<1> reduction_dim{axis};
+    switch (type) {
+    case NormType::L1:
+        norms = input.abs().sum(reduction_dim);
+        break;
+    case NormType::L2:
+        norms = input.square().sum(reduction_dim);
+        break;
+    case NormType::Max:
+        norms = input.abs().maximum(reduction_dim);
+        break;
+    }
+    {
+        tensord<N - 1> eps_tensor(norms.dimensions());
+        eps_tensor.setConstant(eps);
+        norms += eps_tensor;
+    }
+
+    int axis_dims = input.dimensions()[axis];
+    for (int i = 0; i < axis_dims; ++i) {
+        input.chip(i, axis) /= norms;
+    }
+}
+
+/**
+ * @brief Return a normalized copy of a tensor along the given axis.
+ *
+ * @tparam N Dimension of the tensor. Must be known at compile time.
+ * @param input Input tensor to normalize.
+ * @param axis Axis along which the tensor will be normalized.
+ * @param type Normalization type. See bnmf_algs::util::NormType for details.
+ * @param eps Floating point epsilon value to prevent division by 0 errors.
+ *
+ * @return Normalized copy of the input tensor along the given axis
+ */
+template <int N>
+tensord<N> normalized(const tensord<N>& input, size_t axis,
+                      NormType type = NormType::L1, double eps = 1e-50) {
+    tensord<N> out = input;
+    normalize(out, axis, type);
+    return out;
+}
 } // namespace util
 } // namespace bnmf_algs
