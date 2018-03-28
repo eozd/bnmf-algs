30380004d1646f4c3bcce6233c2dba2ed3cafd8f
Esref Ozdemir <esref.ozdemir27@gmail.com>
2018-03-28 12:31:04 +0300

    bench_utils documentation

--- benchmark/bench_utils.hpp
+++ benchmark/bench_utils.hpp
@@ -2,7 +2,37 @@
 
 #include "bnmf_algs.hpp"
 
+/**
+ * @brief benchmark namespace contains common functions and auxiliaries to be
+ * used by different benchmarks.
+ */
 namespace benchmark {
+/**
+ * @brief Construct a new bnmf_algs::matrix_t with given size and element
+ * magnitude.
+ *
+ * Each element in the constructed matrix belongs to the range
+ * [beg, beg + scale).
+ *
+ * @param x Rows of the matrix to construct.
+ * @param y Columns of the matrix to construct.
+ * @param beg Beginning of the entry magnitude range (included).
+ * @param scale Scale of the entry magnitude range.
+ *
+ * @return Constructed matrix.
+ */
 bnmf_algs::matrix_t make_matrix(long x, long y, double beg, double scale);
-bnmf_algs::allocation_model::AllocModelParams make_params(long x, long y, long z);
+
+/**
+ * @brief Construct a new bnmf_algs::allocation_model::AllocModelParams with
+ * parameters as given in \cite kurutmazbayesian.
+ *
+ * @param x Number of rows of matrix \f$X\f$.
+ * @param y Number of columns of matrix \f$X\f.
+ * @param z Decomposition rank.
+ *
+ * @return Allocation model parameters as defined in \cite kurutmazbayesian.
+ */
+bnmf_algs::allocation_model::AllocModelParams make_params(long x, long y,
+                                                          long z);
 } // namespace benchmark
