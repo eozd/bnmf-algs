c2d04d03e5d0246bede1c206893ac18693a3298e
Esref Ozdemir <esref.ozdemir27@gmail.com>
2018-05-11 09:37:01 +0300

    Write W, H, S matrices for movielens

--- movielens/movielens.cpp
+++ movielens/movielens.cpp
@@ -35,8 +35,11 @@ int main(int argc, char** argv) {
         x_orig_file << X;
     }
 
-    shape<3> tensor_shape{n_rows, n_cols, n_components};
-    auto param_vec = alloc_model::make_EM_params<double>(tensor_shape);
+	std::vector<double> alpha(n_rows, 0.01);
+	std::vector<double> beta(n_cols, 1000);
+	double b = 2;
+	alloc_model::Params<double> params(1, b, alpha, beta);
+	std::vector<alloc_model::Params<double>> param_vec(n_components, params);
 
     std::cout << "Computing the factorization" << std::endl;
     auto alg_begin_time = high_resolution_clock::now();
@@ -50,6 +53,28 @@ int main(int argc, char** argv) {
         << duration_cast<milliseconds>(alg_end_time - alg_begin_time).count()
         << " milliseconds" << std::endl;
 
+	for (size_t i = 0; i < em_res.EM_bound.cols(); ++i) {
+		std::cout << em_res.EM_bound(i) << std::endl;
+	}
+
+	matrix_t<double> W = em_res.logW.array().exp();
+    matrix_t<double> H = em_res.logH.array().exp();
+    tensor_t<double, 3> S(n_rows, n_cols, n_components);
+    for (size_t i = 0; i < n_rows; ++i) {
+        for (size_t j = 0; j < n_cols; ++j) {
+            for (size_t k = 0; k < n_components; ++k) {
+                S(i, j, k) = W(i, k) * H(k, j);
+            }
+        }
+    }
+
     std::ofstream x_file("X_full.txt", std::ios_base::trunc);
+    std::ofstream h_file("W.txt", std::ios_base::trunc);
+    std::ofstream w_file("H.txt", std::ios_base::trunc);
+    std::ofstream s_file("S.txt", std::ios_base::trunc);
+
     x_file << em_res.X_full;
+    w_file << W;
+    h_file << H;
+    s_file << S;
 }
